a74c5324dc4aee3778351dd5eaf5549b6371d4bd
TheBobooo <idorbogdan_2005@yahoo.com>
2025-12-22 21:17:22 +0200

    commit

---
+++ src/main/java/com/pass/passwordchecker/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.pass.passwordchecker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:63342", "http://127.0.0.1:63342") // Add your IntelliJ preview port here
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
